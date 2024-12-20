d2599879dd23eb958ba609682857b89b99e5c997
afaq <afaqak124@gmail.com>
2024-12-20 19:31:55 +0500

    updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,14 +6,11 @@ import { asap_condensed } from "@/lib/fonts";
 import { Header } from "@/components/Header";
 import { Toaster } from "sonner";
 
-
 export const metadata: Metadata = {
-  title: "Bizer - Empowering Innovation",
-  description: 
-    "Bizer is a leading tech startup revolutionizing the digital landscape. " +
-    "We specialize in cutting-edge solutions, innovative technologies, and transformative services to help businesses and individuals thrive in the digital age.",
+  title: "HackToast - Empowering Innovation",
+  description:
+    "HackToast is a leading tech startup revolutionizing the digital landscape. We specialize in cutting-edge solutions, innovative technologies, and transformative services to help businesses and individuals thrive in the digital age.",
   keywords: [
-    "Hack",
     "Tech Startup",
     "Innovative Solutions",
     "Technology Services",
@@ -21,8 +18,49 @@ export const metadata: Metadata = {
     "Software Development",
     "AI Solutions",
     "Tech Innovation",
-  ]
- 
+    "HackToast",
+    "Digital Revolution",
+    "Cutting-Edge Technology",
+  ],
+  openGraph: {
+    title: "HackToast - Empowering Innovation",
+    description:
+      "HackToast is a leading tech startup revolutionizing the digital landscape with cutting-edge solutions, innovative technologies, and transformative services.",
+    url: "https://hacktoast.com",
+    siteName: "HackToast",
+    type: "website",
+    images: [
+      {
+        url: "https://hacktoast.com/logo.png",
+        width: 1200,
+        height: 630,
+        alt: "HackToast Logo",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "HackToast - Empowering Innovation",
+    description:
+      "HackToast is a leading tech startup revolutionizing the digital landscape with cutting-edge solutions, innovative technologies, and transformative services.",
+    site: "@hacktoast",
+    creator: "@hacktoast",
+    // images: ["https://hacktoast.com/twitter-card-image.jpg"],
+  },
+  robots: {
+    index: true,
+    follow: true,
+    "max-snippet": -1,
+    "max-image-preview": "large",
+    "max-video-preview": -1,
+  },
+  alternates: {
+    canonical: "https://hacktoast.com",
+    languages: {
+      "en-US": "https://hacktoast.com",
+      // "es-ES": "https://hacktoast.com/es",
+    },
+  },
 };
 
 export default function RootLayout({
@@ -36,7 +74,7 @@ export default function RootLayout({
         <Navbar />
         <Header />
         {children}
-        <Toaster/>
+        <Toaster />
       </body>
     </html>
   );
